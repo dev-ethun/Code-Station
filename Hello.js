5f6f2a6e6a6a77974aea0631e9eb46f53458bb15
Dev-Ethun <ethuntcg5@gmail.com>
2025-01-16 01:13:51 +0600

    update

--- Hello.js
+++ Hello.js
@@ -13,6 +13,25 @@ function home(){
     window.location.href='./index.html'
 }
 
+function Edu(){
+    window.location.href='./Edu.html'
+}
+
+function course(){
+    window.location.href='./course.html'
+}
+
+
+function project(){
+    window.location.href='./project.html'
+}
+
+
 function about(){
     window.location.href='./about.html'
 }
+
+function contact(){
+    window.location.href='./contact.html'
+}
+
