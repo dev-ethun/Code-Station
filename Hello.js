35e588d1b66ae72bc14b6e7169c16ce512fdb9b8
Dev-Ethun <ethuntcg5@gmail.com>
2025-01-15 14:18:54 +0600

    updated

---
+++ Hello.js
@@ -0,0 +1,18 @@
+
+
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+
+function hey(){
+    window.location.href='./hey.html'
+}
+
+function home(){
+    window.location.href='./index.html'
+}
+
+function about(){
+    window.location.href='./about.html'
+}
